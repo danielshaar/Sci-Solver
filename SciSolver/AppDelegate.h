9338c4468a6f5e37498247f1e3ec54392896f77e
Shiv Roychowdhury <shivroychowdhury@Shivs-MacBook-Pro-4.local>
2013-06-15 23:56:42 -0400

    Initial Commit

---
+++ SciSolver/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SciSolver
+//
+//  Created by Shiv Roychowdhury on 6/15/13.
+//  Copyright (c) 2013 Water Works Development. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
